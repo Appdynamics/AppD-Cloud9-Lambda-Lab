09b0cd549999987e8a65572e8429993d1d879ddc
David <davidryder@yahoo.com>
2020-07-20 09:05:07 -0700

    Updates

--- envvars.sh
+++ envvars.sh
@@ -4,7 +4,7 @@ export AWS_CLOUD9_GROUP_NAME="CLOUD9GROUP1"
 export AWS_CLOUD9_USER_NAME="CLOUD9USER1"
 export AWS_CLOUD9_ENV_NAME="CLOUDIDEENV1"
 
-# Only set for testing
+# Only set for testing.
 export AWS_CLOUD9_USER_PWD=""
 
 # Configurtion
