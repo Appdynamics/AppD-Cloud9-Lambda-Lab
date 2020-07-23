0d905fc839777812bb69e0ec485f24fa859ed195
David <davidryder@yahoo.com>
2020-07-23 13:23:52 -0700

    _randomPassword

--- bashUtilities.sh
+++ bashUtilities.sh
@@ -15,3 +15,8 @@ _awsCloud9ListEnvironments() {
     aws cloud9 describe-environments --environment-ids $ENV_ID
   done
 }
+
+_randomPassword() {
+  RND_PWD="$(openssl rand -base64 64 | tr -dc A-Z | cut -c1-4)+$(openssl rand -base64 64 | tr -dc 0-9 | cut -c1-4)-$(openssl rand -base64 64 | tr -dc a-z | cut -c1-4)=$(openssl rand -base64 8 | tr -dc A-Z-a-z-0-9)"
+  echo $RND_PWD
+}
